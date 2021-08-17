e181c36746677917b1d80817a996f4b4e2b3b0ad
shuyun.cheng <shuyun.cheng@100credit.com>
2021-08-17 21:03:14 +0800

    :sparkles: 原型模式

---
+++ src/main/java/com/csy/design/memento/PreviousCalculationToCareTaker.java
@@ -0,0 +1,8 @@
+package com.csy.design.memento;
+
+/**
+ *  Memento interface to CalculatorOperator (Caretaker)
+ */
+public interface PreviousCalculationToCareTaker {
+    // no operations permitted for the caretaker
+}
