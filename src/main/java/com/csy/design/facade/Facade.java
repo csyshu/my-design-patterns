3fd4542e23332299b31f51943dfc6fbb85e1ef37
shuyun.cheng <shuyun.cheng@100credit.com>
2021-08-17 21:07:09 +0800

    :sparkles: 门面模式

---
+++ src/main/java/com/csy/design/facade/Facade.java
@@ -0,0 +1,11 @@
+package com.csy.design.facade;
+
+public class Facade {
+    private final SubSystem subSystem = new SubSystem();
+
+    public void watchMovie() {
+        subSystem.turnOnTV();
+        subSystem.setCD("a movie");
+        subSystem.startWatching();
+    }
+}
